44600871fa87a2d9df7a5aa3e7a9635e5eeb0f73
Piotr Solarski <piotr.solarski@bluesoft.com>
2021-06-11 18:04:39 +0200

    Initialized project

---
+++ src/main/kotlin/org/etag/validator/etagvalidator/EtagValidatorApplication.kt
@@ -0,0 +1,11 @@
+package org.etag.validator.etagvalidator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EtagValidatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<EtagValidatorApplication>(*args)
+}
---
+++ src/test/kotlin/org/etag/validator/etagvalidator/EtagValidatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.etag.validator.etagvalidator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EtagValidatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
